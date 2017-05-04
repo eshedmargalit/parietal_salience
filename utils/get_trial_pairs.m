34c061f63cbf9b3da78446eaed09e6ad10e042f0
Eshed Margalit <eshed.margalit@gmail.com>
2017-05-03 20:53:12 -0700

    new

---
+++ utils/get_trial_pairs.m
@@ -0,0 +1,26 @@
+function pairs = get_trial_pairs(tvec1, tvec2)
+% GET_TRIAL_PAIRS examines image associated with each trial and finds paired trials
+% Inputs
+%	tvec1 - vector of Trial objects
+%	tvec2 - vector of Trial objects
+% Outputs
+% Eshed Margalit
+% May 3, 2017
+
+	pairs = cell(1,2);
+
+	for i = 1:length(tvec1)
+		for j = 1:length(tvec2)
+			t1 = tvec1{i};
+			t2 = tvec2{j};
+
+			fname1 = t1.fname
+			fname2 = t2.fname
+
+			if strcmp(fname1, fname2)
+				pairs{end,1} = t1;
+				pairs{end,2} = t2;
+			end
+		end
+	end
+end
