c958e5fd91d3edf141160af63b9c3970cf413a2a
Eshed Margalit <eshed.margalit@gmail.com>
2017-05-05 12:37:09 -0700

    Added default to read pre-existing dataset file

--- utils/load_experiments.m
+++ utils/load_experiments.m
@@ -7,14 +7,12 @@ function datasets = load_experiments(varargin)
 
 	
 	% Parse varargin
-	if length(varargin) > 1
-		error('Too many arguments.');
-	end
-
 	if isempty(varargin)
 		experiment_numbers = 2:11;
+		force_recompute = 0;
 	else
 		experiment_numbers = varargin{1};
+		force_recompute = varargin{2};
 	end
 
 	% Define constants
@@ -27,6 +25,23 @@ function datasets = load_experiments(varargin)
 	% Loop over all experiments
 	for i=1:length(experiment_numbers)
 		exp_num = experiment_numbers(i);
+
+		ds_str = sprintf('data/processed_datasets/experiment%d.mat',...
+			exp_num);
+
+		if exist(ds_str)
+			fprintf('DataSet already exists for experiment %d.\n',...
+				exp_num);
+			if force_recompute
+				fprintf('Recomputing anyway, override enabled.\n');
+			else
+				dataset = load(ds_str);
+				dataset = dataset.dataset;
+				datasets{i} = dataset;
+				continue;
+			end
+		end
+
 		name = sprintf('Experiment %d',exp_num);
 		fprintf('\nExperiment %d...\n', exp_num);
 
@@ -51,8 +66,10 @@ function datasets = load_experiments(varargin)
 			exp_num, exp_num);
 
 		% Call init() to process trials and fixations
-		datasets{i} = init(trial_info,fixation_info,TrialType,...
+		dataset = init(trial_info,fixation_info,TrialType,...
 			blink_tbl,im_dir,name,exp_num);
+		datasets{i} = dataset;
+		save(ds_str,'dataset');
 	end
 	fprintf('\n');
 end
